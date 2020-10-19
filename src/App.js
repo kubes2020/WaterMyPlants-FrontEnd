c1959e0189bb468bf1a504407e6387a8315a7830
Robert Tierie <tangodolphin13@gmail.com>
2020-10-20 00:19:04 +0200

    signup only

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import styled from 'styled-components';
+// import { Route } from 'react-router-dom';
+// import styled from 'styled-components';
 
 import SignUp from './components/SignUp';
-import Login from './components/Login';
-import Plants from './components/Plants';
+// import Login from './components/Login';
+// import Plants from './components/Plants';
 
 
 
@@ -15,20 +15,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Plants</h1>
+      <SignUp />
     </div>
   );
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,15 +1,103 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import * as yup from 'yup';
-import styled from 'styled-components';
 
 
+export default function Signup() {
+    const [values, setValues] = useState({
+        username: "",
+        password: "",
+    });
 
-// CODE HERE
+    const [errors, setErrors] = useState({
+        username: "",
+        password: "",
+    })
 
 
+    const formSchema = yup.object().shape({
+        username: yup
+        .string()
+        .required(2, "Username is a required field."), 
+        password: yup
+          .string()
+          .required("Must include password."),
+      });
+
+      const [buttonDisabled, setButtonDisabled] = useState(true);
+
+      useEffect(() => {
+        formSchema.isValid(values).then(valid => {
+          setButtonDisabled(!valid);
+        });
+      }, [values]);
 
 
+const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitting ", values);
+        setValues({
+            username: "",
+            password: "",
+        }); 
+    };
 
+    const inputChange = e => {
+        e.persist();
+        setValues({
+          ...values,
+          [e.target.name]: e.target.value
+            // e.target.type === "checkbox" ? e.target.checked : e.target.value
+        });
+    
+        validateChange(e);
+        // setValues(newFormData);
+      };
 
-export default Signup
+      const validateChange = e => {
+        // Reach will allow us to "reach" into the schema and test only one part.
+        yup
+          .reach(formSchema, e.target.name)
+          .validate(e.target.value)
+          .then(valid => {
+            setErrors({
+              ...errors,
+              [e.target.name]: ""
+            });
+          })
+          .catch(err => {
+            setErrors({
+              ...errors,
+              [e.target.name]: err.errors[0]
+            });
+          });
+      };
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="username">Enter your Username
+                    <input 
+                    type="text"
+                    name="username"
+                    placeholder="username"
+                    values={values.username}
+                    onChange={inputChange}
+                    />
+                </label>
+            </div>
+            <div>
+                <label htmlFor="password">Enter a password
+                    <input 
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    values={values.password}
+                    onChange={inputChange}
+
+                    />
+                </label>
+            </div>
+            <button disabled={buttonDisabled} type='submit'>Sign Up</button>
+        </form>
+    )
+}
