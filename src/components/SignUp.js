0dbb84e31406dca7f976662bcc62587a1e3d249d
Brian Kubes <68407161+kubes2020@users.noreply.github.com>
2020-10-21 15:53:49 -0700

    Update SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,32 +1,43 @@
 import React, { useState, useEffect } from 'react';
+
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import * as yup from 'yup';
 import { Link } from "react-router-dom";
-import "../style/signup-login.css";
+
+
 
 export default function SignUp(props) {
     const [values, setValues] = useState({
         username: "",
         password: "",
     });
+
+
     const [errors, setErrors] = useState({
         username: "",
         password: "",
     })
+
+
     const formSchema = yup.object().shape({
         username: yup
         .string()
-        .required(2, "Username is a required field."),
+        .required(2, "Username is a required field."), 
         password: yup
           .string()
           .required("Must include password."),
       });
+
       const [buttonDisabled, setButtonDisabled] = useState(true);
+
       useEffect(() => {
         formSchema.isValid(values).then(valid => {
           setButtonDisabled(!valid);
         });
       }, [values]);
+
+
+
 const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Submitting ", values);
@@ -38,20 +49,26 @@ const handleSubmit = (e) => {
         setValues({
             username: "",
             password: "",
-        });
+        }); 
     })
     .catch(err => {
         console.log("error with SignUp", err)
-    })
+    })    
     };
+
+
     const inputChange = e => {
         e.persist();
         setValues({
           ...values,
           [e.target.name]: e.target.value
+
         });
+    
         validateChange(e);
+
       };
+
       const validateChange = e => {
         // Reach will allow us to "reach" into the schema and test only one part.
         yup
@@ -72,35 +89,37 @@ const handleSubmit = (e) => {
       };
     return (
       <>
-      <div class="container">
         <form onSubmit={handleSubmit}>
           <div>
-            <h1 class="h3 mb-3 font-weight-normal">
-              Sign up here.
+            <h1>
+              Sign up here. 
             </h1>
-
+            
           </div>
             <div>
-                <label htmlFor="username">Create a: </label>
-                    <input
+                <label htmlFor="username">
+                    <input 
                     type="text"
                     name="username"
                     placeholder="Username"
                     value={values.username}
                     onChange={inputChange}
+
                     />
-                
+                </label>
             </div>
             <div>
-                <label htmlFor="password">Create a:  </label>
-                    <input
+
+                <label htmlFor="password">
+                    <input 
                     type="password"
                     name="password"
                     placeholder="Password"
                     value={values.password}
                     onChange={inputChange}
+
                     />
-               
+                </label>
             </div>
             <p>
             By selecting Agree and continue below, I agree to Water My Plants' Terms and Conditions.
@@ -109,13 +128,7 @@ const handleSubmit = (e) => {
             <p>
             <p className="text-link">Already have an account? <Link to="/Login">Log in</Link></p>
             </p>
-
         </form>
-      </div>
-
         </>
     )
 }
-
-
-
