d2d28e54800718f3371c156857955671777eef66
DavidHall-Code <david_m_hall@outlook.com>
2020-10-21 13:04:05 -0400

    Sass foler and css forlder for bootstrap

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,21 @@
+// import React from "react";
+
+// export const Login = () => {
+
+//   return(
+//     <>
+//     <h2>Login Page</h2>
+//     </>
+//   )
+// }
+
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+  
+ 
+  
+
 import * as yup from "yup";
 
 const formSchema = yup.object().shape({
@@ -10,18 +25,23 @@ const formSchema = yup.object().shape({
     .required("Must include your password."),
 });
 
-export default function Login(props) {
+export default function Login() {
+  
   const [buttonDisabled, setButtonDisabled] = useState(true);
- 
+
+  
   const [formState, setFormState] = useState({
     username: "",
     password: "",  
   });
 
+  
   const [errors, setErrors] = useState({
     username: "",
     password: "",
   });
+
+ 
 
   useEffect(() => {
     formSchema.isValid(formState).then(valid => {
@@ -29,28 +49,13 @@ export default function Login(props) {
     });
   }, [formState]);
 
-  const handleSubmit = e => {
+  const formSubmit = e => {
     e.preventDefault();
-    console.log("Submitting ", formState);
-    axiosWithAuth().post("/auth/login", formState)
-    .then(res => {
-        console.log("res from Login", res)
-        window.localStorage.setItem('token', res.data.token)
-        window.localStorage.setItem('id', res.data.user.id)
-        props.setIsLoggedIn(true)
-        props.history.push("/plantcard")
-        setFormState({
-            username: "",
-            password: "",
-        }); 
-    })
-    .catch(err => {
-        console.log("error with Login", err)
-    })    
     
   };
 
   const validateChange = e => {
+    
     yup
       .reach(formSchema, e.target.name)
       .validate(e.target.value)
@@ -82,23 +87,23 @@ export default function Login(props) {
 
   return (
     
-<form onSubmit={handleSubmit}>
+<form onSubmit={formSubmit}>
     <div className="form-header">
         <h1>Water My Plants Login</h1>
         <p>Welcome, lets keep your plants happy</p>
     </div>
 
     <label htmlFor="name">Username</label>
-    <input type="text" name="username" value={formState.username} onChange={inputChange} placeholder="Enter your Username" />
+    <input type="text" name="username" value={formState.username} onChange={inputChange} placeHolder="Enter your Username" />
 
     <label htmlFor="password">Password</label>
-    <input type="password" name="password" value={formState.password} onChange={inputChange} placeholder="Enter your Password" />
+    <input type="text" name="password" value={formState.password} onChange={inputChange} placeHolder="Enter your Password" />
           
 
 
     <button disabled={buttonDisabled}>Login</button>
 
-    <p className="text-link">Not a member yet? <Link to="/signup">Sign-up here</Link></p>
+    <p className="text-link">Not a member yet? <Link to="/SignUp">Sign-up here</Link></p>
 </form>
     
   );
