7b19689a1b6826298abdc9bd039aae21c20c714b
Brian Kubes <68407161+kubes2020@users.noreply.github.com>
2020-10-21 15:51:30 -0700

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,6 @@
-// import React from "react";
-
-// export const Login = () => {
-
-//   return(
-//     <>
-//     <h2>Login Page</h2>
-//     </>
-//   )
-// }
-
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "../style/signup-login.css";
-  
- 
-  
-
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 import * as yup from "yup";
 
 const formSchema = yup.object().shape({
@@ -25,23 +10,18 @@ const formSchema = yup.object().shape({
     .required("Must include your password."),
 });
 
-export default function Login() {
-  
+export default function Login(props) {
   const [buttonDisabled, setButtonDisabled] = useState(true);
-
-  
+ 
   const [formState, setFormState] = useState({
     username: "",
     password: "",  
   });
 
-  
   const [errors, setErrors] = useState({
     username: "",
     password: "",
   });
-
- 
 
   useEffect(() => {
     formSchema.isValid(formState).then(valid => {
@@ -49,9 +29,8 @@ export default function Login() {
     });
   }, [formState]);
 
-  const formSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-
     console.log("Submitting ", formState);
     axiosWithAuth().post("/auth/login", formState)
     .then(res => {
@@ -69,12 +48,10 @@ export default function Login() {
     .catch(err => {
         console.log("error with Login", err)
     })    
-
     
   };
 
   const validateChange = e => {
-    
     yup
       .reach(formSchema, e.target.name)
       .validate(e.target.value)
@@ -105,33 +82,26 @@ export default function Login() {
   };
 
   return (
-  <div class="container">   
-    <form onSubmit={formSubmit}>
-      <div className="form-header">
-        <h1>Login Here </h1>
-        
-        <h3>Lets keep your plants happy</h3>
-     </div>
-
-    <div>
-      <label htmlFor="name">Username: </label>
-      <input type="text" name="username" value={formState.username} onChange={inputChange} placeHolder="Enter your Username" />
-    </div>
     
-    <br></br>
+<form onSubmit={handleSubmit}>
+    <div className="form-header">
+        <h1>Water My Plants Login</h1>
+        <p>Welcome, lets keep your plants happy</p>
+    </div>
 
-    <div>
-      <label htmlFor="password">Password: </label>
-      <input type="text" name="password" value={formState.password} onChange={inputChange} placeHolder="Enter your Password" />
-    </div> 
+    <label htmlFor="name">Username</label>
+    <input type="text" name="username" value={formState.username} onChange={inputChange} placeholder="Enter your Username" />
 
-    <br></br>    
-   
+    <label htmlFor="password">Password</label>
+    <input type="password" name="password" value={formState.password} onChange={inputChange} placeholder="Enter your Password" />
+          
+
+
     <button disabled={buttonDisabled}>Login</button>
 
-    <p className="text-link">Not a member yet? <Link to="/SignUp">Sign-up here</Link></p>
-    </form>
-  </div> 
+    <p className="text-link">Not a member yet? <Link to="/signup">Sign-up here</Link></p>
+</form>
+    
   );
 }
 
