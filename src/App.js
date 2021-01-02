2041510e56c96a08d7b0c835006f774908f5017a
Brian Kubes <briankubes@gmail.com>
2021-01-01 22:53:31 -0800

    minor change

--- src/App.js
+++ src/App.js
@@ -25,8 +25,7 @@ const NavLink = styled(Link)`
   text-decoration: none;
   padding: .4% 0;
   color: #F8E30C;
-  background: #81A99D;
-  
+  background: #81A99D; 
   min-width: 150px;
   margin: .2%;
   font-size: 1.2rem;
