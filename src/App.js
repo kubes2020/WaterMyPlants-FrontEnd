9d77c66e020c108b7b53ca876dd2b379eb1d56f8
Brian Kubes <briankubes@gmail.com>
2021-03-04 16:20:09 -0800

    test which repo this is pushing to

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-
-import React, { useState } from 'react';
+// this comment is a test and should be removed
+import React, { useState } from "react";
 import { Home } from "./components/Home";
-import { Route, Link, useHistory } from 'react-router-dom';
+import { Route, Link, useHistory } from "react-router-dom";
 import AddPlants from "./components/AddPlants";
 import { PrivateRoute } from "./components/PrivateRoute";
 import Login from "./components/Login";
 import PlantCard from "./components/PlantCard";
-import SignUp from './components/SignUp';
+import SignUp from "./components/SignUp";
 import EditPlant from "./components/EditPlant";
-import {PlantContext} from "./components/contexts/PlantContext";
+import { PlantContext } from "./components/contexts/PlantContext";
 import Settings from "./components/Settings";
 import styled from "styled-components";
-import './App.css';
-
+import "./App.css";
 
 // styled components
 const MainNav = styled.nav`
   text-align: right;
-  @media (max-width: 400px){
+  @media (max-width: 400px) {
     text-align: center;
   }
-`
+`;
 const NavLink = styled(Link)`
   text-decoration: none;
-  padding: .4% 0;
-  color: #F8E30C;
-  background: #81A99D; 
+  padding: 0.4% 0;
+  color: #f8e30c;
+  background: #81a99d;
   min-width: 150px;
-  margin: .2%;
+  margin: 0.2%;
   font-size: 1.2rem;
   justify-content: center;
   border-radius: 5px;
@@ -38,43 +37,55 @@ const NavLink = styled(Link)`
     background-color: darkgreen;
     transition: all 0.5s ease-in-out;
   }
-  @media (max-width: 400px){
+  @media (max-width: 400px) {
     font-size: 1rem;
     flex-direction: column;
   }
-  
-`
+`;
 
 function App() {
-  const history = useHistory()
-  const [plantId, setPlantId] = useState()
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const history = useHistory();
+  const [plantId, setPlantId] = useState();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleLogOut = () =>{
-    localStorage.clear()
-    setIsLoggedIn(false)
-    history.push("/")
-  }
+  const handleLogOut = () => {
+    localStorage.clear();
+    setIsLoggedIn(false);
+    history.push("/");
+  };
 
   return (
     <div className="App">
-
-      <MainNav>      
-        <NavLink to="/">Home</NavLink>     
+      <MainNav>
+        <NavLink to="/">Home</NavLink>
         {isLoggedIn ? <NavLink to="/settings">User Settings</NavLink> : null}
         {isLoggedIn ? <NavLink to="/plantcard">View My Plants</NavLink> : null}
         {isLoggedIn ? <NavLink to="/addplants">Add Plants</NavLink> : null}
         {isLoggedIn ? null : <NavLink to="/login">Login</NavLink>}
-        {isLoggedIn ? <NavLink to="/" onClick={ handleLogOut }>Logout</NavLink> : null}  
+        {isLoggedIn ? (
+          <NavLink to="/" onClick={handleLogOut}>
+            Logout
+          </NavLink>
+        ) : null}
       </MainNav>
-      <PlantContext.Provider value={{plantId, setPlantId}}>
+      <PlantContext.Provider value={{ plantId, setPlantId }}>
         <Route exact path="/" component={Home}></Route>
-        <Route exact path="/login" render={(props) => <Login {...props} setIsLoggedIn={setIsLoggedIn}/>}></Route>
-        <Route exact path="/signup" render={(props) => <SignUp {...props} setIsLoggedIn={setIsLoggedIn}/>}></Route>
-        <PrivateRoute exact path="/addplants" component={AddPlants}/>
-        <PrivateRoute exact path="/plantcard" component={PlantCard}/>
-        <PrivateRoute exact path="/editplant" component={EditPlant}/>
-        <PrivateRoute exact path="/settings" component={Settings}/>  
+        <Route
+          exact
+          path="/login"
+          render={(props) => <Login {...props} setIsLoggedIn={setIsLoggedIn} />}
+        ></Route>
+        <Route
+          exact
+          path="/signup"
+          render={(props) => (
+            <SignUp {...props} setIsLoggedIn={setIsLoggedIn} />
+          )}
+        ></Route>
+        <PrivateRoute exact path="/addplants" component={AddPlants} />
+        <PrivateRoute exact path="/plantcard" component={PlantCard} />
+        <PrivateRoute exact path="/editplant" component={EditPlant} />
+        <PrivateRoute exact path="/settings" component={Settings} />
       </PlantContext.Provider>
     </div>
   );
